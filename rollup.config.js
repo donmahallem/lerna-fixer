5489582aaf8cd4b489c0e423d5a2be6a9e9f83ad
DonMahallem <donmahallem@users.noreply.github.com>
2022-01-07 12:28:57 +0100

    feat: add library mode

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,35 @@
 import defaultRollup from '@donmahallem/rollup-config';
 import pkg from './package.json';
+import magicstring from 'magic-string';
 
-export default defaultRollup(pkg, {
+const d = defaultRollup(pkg, {
     output: {
         cjs: false,
     },
     tsconfig: './tsconfig.app.json'
 });
+
+d.input = {
+    'cli': './src/cli.ts',
+    'index': './src/index.ts'
+}
+d.output[0].file = undefined;
+d.output[0].dir = './dist'
+d.output[0].entryFileNames = '[name].mjs'
+d.output[0].chunkFileNames = '[name]-[hash].mjs'
+d.plugins.push({
+    name: 'addShebang',
+    renderChunk(code, { fileName }, { dir, file, sourcemap }) {
+        const shebang = '#!/usr/bin/env node\n\n';
+        if (!fileName.endsWith('cli.mjs')) {
+            return;
+        }
+        const parsed = new magicstring(code);
+        parsed.prepend(shebang);
+        return {
+            code: parsed.toString(),
+            map: parsed.generateMap(),
+        };
+    }
+})
+export default d;
